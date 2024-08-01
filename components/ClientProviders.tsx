7bbea4bf9ce7b235bbda968501c44fd0e59aa8b4
t-whelan <whelan.tionne@gmail.com>
2024-08-01 13:37:12 +0200

    register user and validation

---
+++ components/ClientProviders.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+import { Toaster } from "react-hot-toast";
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+}
